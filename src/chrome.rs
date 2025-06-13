24106631a17c266c74d77911c33e2ecaadc3c97a
programingjd <programingjd@gmail.com>
2025-06-13 13:33:15 +0200

    -

--- src/chrome.rs
+++ src/chrome.rs
@@ -1,5 +1,6 @@
 use crate::http_client::json_client;
 use pinboard::Pinboard;
+use serde::Deserialize;
 use std::sync::LazyLock;
 use tracing::debug;
 
@@ -13,40 +14,51 @@ pub(crate) struct ChromeVersion {
 pub(crate) static CHROME_VERSION: LazyLock<Pinboard<ChromeVersion>> =
     LazyLock::new(Pinboard::new_empty);
 
+#[derive(Deserialize)]
+struct Release {
+    milestone: u16,
+}
+
 pub(crate) async fn update_chrome_version(timestamp: u32) -> bool {
     match json_client()
-        .get("https://raw.githubusercontent.com/chromium/chromium/main/chrome/VERSION")
+        .get("https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Windows&num=1&offset=0")
         .send()
         .await
     {
-        Ok(response) => match response.text().await {
-            Ok(text) => {
-                match text.lines().next().and_then(|it| {
-                    let mut split = it.split('=');
-                    let _ = split.next();
-                    split.next().and_then(|it| it.parse::<u16>().ok())
-                }) {
-                    Some(chrome_version) => {
-                        CHROME_VERSION.set(ChromeVersion {
-                            chrome_version: chrome_version - 2,
-                            timestamp,
-                        });
-                        true
-                    }
-                    None => {
-                        debug!("failed to parse chrome version");
-                        false
-                    }
+        Ok(response) => match response.json::<Vec<Release>>().await {
+            Ok(it) => {
+                if it.is_empty() {
+                    debug!("failed to get chrome version");
+                    return false;
                 }
+                CHROME_VERSION.set(ChromeVersion {
+                    chrome_version: it.into_iter().next().map(|it| it.milestone).unwrap(),
+                    timestamp,
+                });
+                true
             }
             Err(err) => {
-                debug!("failed to get chrome verson file from github:\n{err:?}");
+                debug!("failed to get chrome version:\n{err:?}");
                 false
             }
         },
         Err(err) => {
-            debug!("failed to get response from github for the chrome verson file:\n{err:?}");
+            debug!("failed to get response from chromiumdash for the latest chrome version:\n{err:?}");
             false
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_update_chrome_version() {
+        assert!(update_chrome_version(0).await);
+        println!(
+            "latest stable chrome version: {}",
+            CHROME_VERSION.get_ref().unwrap().chrome_version
+        );
     }
 }
